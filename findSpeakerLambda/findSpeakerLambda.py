33ad800a54180fd81469152fd91965e7c1278689
Paul Iusztin <p.e.iusztin@gmail.com>
2018-07-24 21:19:16 +0300

    A queue demo logic

---
+++ findSpeakerLambda/findSpeakerLambda.py
@@ -0,0 +1,64 @@
+import redis 
+
+'''LISTENER SCRIPT that has the find speaker logic'''
+
+REDIS_SETUP = {
+'host' : 'lowkeyapp-redis-001.hznbrp.0001.euc1.cache.amazonaws.com',
+'port': 6379,
+'db': 0,
+}
+
+def handler(event, context):
+    rq = RedisQueue('queue', **REDIS_SETUP)
+    user = event["user"]
+    rq.put(user)
+
+    while rq.get_redis_client().get(user) is None:
+        pass
+
+    return rq.get_redis_client().get(user).decode('utf-8')
+
+
+
+
+class RedisQueue(object):
+    """Simple Queue with Redis Backend"""
+    def __init__(self, name, namespace='queue', **redis_kwargs):
+        """The default connection parameters are: host='localhost', port=6379, db=0"""
+        self.__db= redis.StrictRedis(**redis_kwargs)
+        self.key = '%s:%s' %(namespace, name)
+
+    def qsize(self):
+        """Return the approximate size of the queue."""
+        return self.__db.llen(self.key)
+
+    def empty(self):
+        """Return True if the queue is empty, False otherwise."""
+        return self.qsize() == 0
+
+    def put(self, item):
+        """Put item into the queue."""
+        self.__db.rpush(self.key, item)
+
+    def get(self, block=True, timeout=None):
+        """Remove and return an item from the queue. 
+
+        If optional args block is true and timeout is None (the default), block
+        if necessary until an item is available."""
+        if block:
+            item = self.__db.blpop(self.key, timeout=timeout)
+        else:
+            item = self.__db.lpop(self.key)
+
+        if item:
+            item = item[1]
+        return item
+        
+    def get_redis_client(self):
+        return self.__db
+
+    def get_nowait(self):
+        """Equivalent to get(False)."""
+        return self.get(False)
+
+    
